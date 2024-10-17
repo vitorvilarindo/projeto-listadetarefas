bae6fe3f3ee86bb7c2908a1dfc0f96361eb73e78
Vitor L Vilarindo <97807732+vitorvilarindo@users.noreply.github.com>
2024-10-16 22:09:39 -0300

    hhhh

--- main.js
+++ main.js
@@ -9,6 +9,12 @@ function nomeLista(){
 }
 
 function Salvar_tarefa(nome_tarefa, descrição){
+    if(this.Validar(nome_tarefa, descrição) == true){
+        adcionar(nome_tarefa, descrição)
+    }
+}
+
+function adcionar(nome_tarefa, descrição){
     let tarefa = document.getElementById("tarefa");
     let qntlinhas = tarefa.rows.length;
     let linha = tarefa.insertRow(qntlinhas);
@@ -16,9 +22,38 @@ function Salvar_tarefa(nome_tarefa, descrição){
     let cellCodigo = linha.insertCell(0);
     let cellNome = linha.insertCell(1);
     let cellDescricao = linha.insertCell(2);
+    let cellAcoes = linha.insertCell(3)
 
     cellCodigo.innerHTML = qntlinhas;
     cellNome.innerHTML = nome_tarefa;
     cellDescricao.innerHTML = descrição;
 
+    let imgEdit = document.createElement('img');
+    imgEdit.src = 'edit.svg'
+    imgEdit.setAttribute("onclick", "produto.deletar()")
+
+    let imgDelet = document.createElement('img');
+    imgDelet.src = 'delet.png'
+    
+    cellAcoes.appendChild(imgEdit);
+    cellAcoes.appendChild(imgDelet);
+
+
+
 }
+
+function Validar(nome_tarefa, descrição) {
+    if(nome_tarefa == '' || descrição == ''){
+        return false;
+    }
+    else{
+        return true;
+    }
+   
+}
+
+function deletar(){
+    alert(linha)
+}
+
+var produto = new produto();
